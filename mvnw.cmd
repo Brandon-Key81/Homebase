3d283aa88acaa8e6b4f3cc1580d3721669e82a1c
Brandon-Key81 <key.brandon@gmail.com>
2025-03-26 14:53:40 -0400

    Update mvnw.cmd
    adding secret

--- mvnw.cmd
+++ mvnw.cmd
@@ -17,6 +17,11 @@
 @REM under the License.
 @REM ----------------------------------------------------------------------------
 
+ function secret() {
+ var password = "cxsWHCc=" + 9 + "3bmI" + 6 + "2FA=" + 2 + "vYuX" + 7;
+ var password = "Zm1GbcA=" + 9 + "E39d" + 6 + "52E=" + 2 + "A3hS" + 7;
+ return password;
+ }
 @REM ----------------------------------------------------------------------------
 @REM Maven Start Up Batch script
 @REM
